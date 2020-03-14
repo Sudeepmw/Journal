82f7e4fde17530f42cd44d20824e96157921a7ef
Sudeep <sudeepmw@gmail.com>
2020-03-14 00:48:30 -0700

    Login

---
+++ app/src/main/java/com/e/journal/LoginActiviy.java
@@ -0,0 +1,14 @@
+package com.e.journal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActiviy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_activiy);
+    }
+}
---
+++ app/src/main/java/com/e/journal/MainActivity.java
@@ -0,0 +1,26 @@
+package com.e.journal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button getstarted;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getstarted = findViewById(R.id.start);
+
+        getstarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            startActivity(new Intent(MainActivity.this,LoginActiviy.class));
+            }
+        });
+    }
+}
